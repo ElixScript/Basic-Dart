02b3e3dcc9fd968c16782afc0f3b3eca57a9558a
mobssspro <156645526+mobssspro@users.noreply.github.com>
2024-01-17 09:01:58 +0700

    Add files via upload

---
+++ tugas-2-typedata/tugas2-typedata.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+void main(){
+  //soal nomer 1
+  //var word = 'dart';
+  //var second = 'is';
+  //var third = 'awesome';
+  //var fourth = 'and';
+  //var fifth = 'I';
+  //var sixth = 'love';
+  //var seventh = 'it!';
+
+  //var sentence = word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh;
+
+  // print(sentence);
+
+  //soal nomer 2
+  //var sentence = "I am going to be Flutter Developer";
+
+  //var words = sentence.split(' ');
+
+  //var exampleFirstWord = words[0];
+  //var exampleSecondWord = words[1];
+  //var thirdWord = words[2];
+  //var fourthWord = words[3];
+  //var fifthWord = words[4];
+  //var sixthWord = words[5];
+  //var seventhWord = words[6];
+
+  //print('First Word: ' + exampleFirstWord);
+  //print('Second Word: ' + exampleSecondWord);
+  //print('Third Word: ' + thirdWord);
+  //print('Fourth Word: ' + fourthWord);
+  //print('Fifth Word: ' + fifthWord);
+  //print('Sixth Word: ' + sixthWord);
+  //print('Seventh Word: ' + seventhWord);
+
+  // soal nomer 3
+  
+  //stdout.write('Masukkan nama depan: ');
+  //String? firstName = stdin.readLineSync();
+
+  //stdout.write('Masukkan nama belakang: ');
+  //String? lastName = stdin.readLineSync();
+
+  //String fullName = '${firstName ?? ''} ${lastName ?? ''}';
+
+  //print('Nama lengkap anda adalah: $fullName');
+
+  // soal nomer 4
+  //stdout.write('Masukkan nilai a: ');
+  //double? a = double.tryParse(stdin.readLineSync() ?? '');
+
+  //stdout.write('Masukkan nilai b: ');
+  //double? b = double.tryParse(stdin.readLineSync() ?? '');
+
+  //if (a == null || b == null) {
+   // print('Input tidak valid. Pastikan Anda memasukkan angka.');
+   // return;
+  //}
+
+  //double multiplication = a * b;
+  //print('Hasil perkalian a * b: $multiplication');
+
+  //double addition = a + b;
+  //print('Hasil penjumlahan a + b: $addition');
+
+ // double subtraction = a - b;
+ // print('Hasil pengurangan a - b: $subtraction');
+
+ // if (b != 0) {
+    //double division = a / b;
+    //print('Hasil pembagian a / b: $division');
+  //} else {
+    //print('Pembagian oleh nol tidak diizinkan.');
+  //}
+
+
+
+
+ 
+
+
+
+}
